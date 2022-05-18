c93ee9eafc84fd1641a5c8fe38a7c9b38de7cf82
Br-Fernandes <93941255+Br-Fernandes@users.noreply.github.com>
2022-05-18 19:40:40 -0300

    Add files via upload

---
+++ src/ListaEncadeada/src/pilhas/ExerciciosPilha.java
@@ -0,0 +1,31 @@
+package pilhas;
+
+import ListaLigada.ListaLigada;
+
+public class ExerciciosPilha {
+
+    Pilha<Integer> pilha;
+
+    public void inverterPilha() {
+        pilha = new Pilha<>();
+        ListaLigada[] elementos = new ListaLigada[pilha.getTopo()];
+
+        if (pilha.isEmpty()) {
+            System.out.println("Pilha vazia");
+
+        } else {
+            for (int i =0;i< pilha.getTopo();i++) {
+                while (pilha.isEmpty()) {
+                    elementos[i] = pilha.getElemento();
+                    pilha.pop();
+
+                }
+
+            }
+
+
+        }
+
+    }
+
+}
